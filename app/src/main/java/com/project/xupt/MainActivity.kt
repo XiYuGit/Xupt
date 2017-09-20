2d320a2813beb006376ef478ca4efb0d479b33b1
xiyu <xiyu03@gmail.com>
2017-09-20 12:55:06 +0800

    first commit

--- app/src/main/java/com/project/xupt/MainActivity.kt
+++ app/src/main/java/com/project/xupt/MainActivity.kt
@@ -2,9 +2,10 @@ package com.project.xupt
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var c:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
