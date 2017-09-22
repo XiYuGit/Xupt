62fba1545ecf3d549e3c61a1972b9a850dc2eb57
xiyu <xiyu03@gmail.com>
2017-09-22 11:00:11 +0800

    first commit

--- app/src/main/java/com/project/xupt/MainActivity.kt
+++ app/src/main/java/com/project/xupt/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.project.xupt
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import com.project.xupt.View.MainUI
 
-class MainActivity : AppCompatActivity() {
-    lateinit var c:TextView
+class MainActivity : AppCompatActivity(),View.OnClickListener{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var textView:TextView
+        textView= findViewById(R.id.denglu) as TextView
+        textView.setOnClickListener {
+            var intent =Intent(this,MainUI)
+            intent.setClass(this,MainUI::class.java)
+        }
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
