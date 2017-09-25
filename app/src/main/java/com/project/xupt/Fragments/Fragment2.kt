9db22964faa1a18453a0663c5e1a474d44e898bd
xiyu <xiyu03@gmail.com>
2017-09-25 18:15:01 +0800

    first commit

--- app/src/main/java/com/project/xupt/Fragments/Fragment2.kt
+++ app/src/main/java/com/project/xupt/Fragments/Fragment2.kt
@@ -5,15 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_1classes.*
 
 /**
  * Created by xiyu0 on 2017/9/25.
  */
-class Fragment1 : Fragment() {
+class Fragment2: Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        var view =inflater.inflate(fragMent1,false
-        )
-        return view
+        var view =inflater.inflate(R.layout.fragMent2)
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
